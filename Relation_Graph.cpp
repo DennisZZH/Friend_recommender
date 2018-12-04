770d17070dee67aa1d33fff48a5b8d0547212ab7
jiajunwan <jiajunwan@umail.ucsb.edu>
2018-12-03 17:52:14 -0800

    Completed and tested Relation_Graph.

--- Relation_Graph.cpp
+++ Relation_Graph.cpp
@@ -8,11 +8,15 @@ void Relation_Graph:: insert_relation(int MyPerm, vector<int> friends) {
 	entry.push_back(MyPerm);    //pushback the first MyPerm element in the first place
 	entry.insert(entry.end(), friends.begin(), friends.end());  //concatenate the friends vector with the entry vector
 	friendship_graph.push_back(entry);  //pushback the entry vector in the adjacency list vector
+	bool contains = false;
 
 	for (int FriendPerm : friends) {    //loop through the friends to find if a previous entry needs to be updated
-		for (vector<int> ListEntry : friendship_graph) {
+		for (vector<int> &ListEntry : friendship_graph) {
 			if (ListEntry[0] == FriendPerm) {
-				ListEntry.push_back(MyPerm);
+				for (int element : ListEntry) {
+					if (element == MyPerm) {contains = true;}
+				}
+				if (!contains) {ListEntry.push_back(MyPerm);}
 			}
 		}
 	}
@@ -30,7 +34,7 @@ int Relation_Graph:: get_index(int Perm) {
 }
 
 void Relation_Graph:: print_graph_test() {
-	for (const vector<int> &entry : friendship_graph) {
+	for (vector<int> entry : friendship_graph) {
 		for (int element : entry) {
 			cout << element << "->";
 		}
